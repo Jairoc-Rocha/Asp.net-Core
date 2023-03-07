8a3c540e50a9d82943c629964981e5714aba9a3a
Jairo Rocha <jairoc.rocha10@gmail.com>
2023-03-06 21:19:25 -0300

    Aprendendo sobre Model

---
+++ LojaWeb/Models/ModeloDados/ClienteModel.cs
@@ -0,0 +1,13 @@
+﻿namespace LojaWeb.Models.ModeloDados
+{
+    public class ClienteModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }    
+        public string CPF { get; set; }
+        public string Telefone { get; set; }
+        public string Endereço { get; set; }
+
+
+    }
+}
